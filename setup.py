9f1e5b79e8bd447e7611554bc0ea8c02028c108f
gurekam <bedi61@uwindsor.ca>
2025-04-13 15:35:26 -0400

    LAP-23: Update package configuration and CI/CD workflow

--- setup.py
+++ setup.py
@@ -1,14 +1,17 @@
 from setuptools import find_packages, setup
 
 setup(
-    name="sales-tracker",
+    name="spreadsheet-application",
     version="0.1",
-    packages=find_packages(),
+    packages=find_packages(include=["backend*"]),
     install_requires=[
         "flask",
+        "flask-socketio",
         "duckdb",
         "redis",
         "werkzeug",
+        "python-socketio",
+        "eventlet",
     ],
     python_requires=">=3.7",
 )
