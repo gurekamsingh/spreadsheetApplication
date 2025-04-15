938e7493c6bcf3fab2ba4664f9ea0c3d5eb0a85e
gurekam <bedi61@uwindsor.ca>
2025-04-15 19:42:03 -0400

    LAP-26: updated the __init__.py file to add all the config variables

--- backend/config/__init__.py
+++ backend/config/__init__.py
@@ -1,1 +1,17 @@
-"""Configuration package initialization.""" 
+"""Configuration package initialization."""
+
+from .config import (
+    DEBUG,
+    HOST,
+    PORT,
+    SECRET_KEY,
+    STATIC_FOLDER,
+    DB_PATH,
+    REDIS_HOST,
+    REDIS_PORT,
+    REDIS_DB,
+    REDIS_TIMEOUT,
+    LOCK_KEY,
+    ACTIVE_USERS_KEY,
+    WRITE_QUEUE_KEY,
+) 
